c95ad0ac2935dccd5eba21bf00c25a85cfb7a25f
AugustoGodoy <augustog5896@gmail.com>
2021-09-05 03:09:35 -0300

    modifify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,16 +9,9 @@ module.exports = {
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
-        'no-console': 'off',
     }
 };
